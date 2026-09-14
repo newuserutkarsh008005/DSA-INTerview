4b922afd108798ef9af1514994da4b43dc2a9bc9
Utkarsh Prakash <utkarshprakash081105@gmail.com>
2026-09-14 07:52:12 +0530

    Cycle detected for undirected graph

--- Graph/Cycle Detection/Cycle_in_undirected_graph.cpp
+++ Graph/Cycle Detection/Cycle_in_undirected_graph.cpp
@@ -1,66 +1,49 @@
-#include <iostream>
-#include <vector>
+#include<iostream>
 using namespace std;
+bool dfs(vector<vector<int>>&a,vector<bool>&visited,int cn,int parent,int n){
+visited[cn]=true;
 
-bool dfs(vector<vector<int>>& a, int node, int parent,
-         vector<bool>& visited, int n) {
-
-    visited[node] = true;
-
-    for (int x = 0; x < n; x++) {
-
-        if (a[node][x] == 0)
-            continue;
-
-        // Ignore the edge back to our parent
-        if (x == parent)
-            continue;
-
-        // Already visited => cycle
-        if (visited[x])
-            return true;
-
-        if (dfs(a, x, node, visited, n))
-            return true;
+for(int i=0;i<n;i++){
+   if(a[cn][i]==1){
+    if(visited[i]==false){
+        if(dfs(a,visited,i,cn,n))return true;
     }
+    else if(parent !=i){
+        return true;
+    }}
 
-    return false;
+   
+}
+return false;
 }
 
-int main() {
+int main(){
 
-    int n = 3;
+    vector<vector<int>>edge={{0,1},{1,2},{2,0}};
+    int n=3;
+    vector<vector<int>>a(n,vector<int>(n,0));
+    vector<bool>visited(n,false);
+    for(int i=0;i<edge.size();i++){
+        int src=edge[i][0];
+        int dest=edge[i][1];
 
-    vector<vector<int>> edge = {
-        {0, 1},
-        {1, 2},
-        {2, 0}
-    };
-
-    vector<vector<int>> a(n, vector<int>(n, 0));
-
-    for (auto& e : edge) {
-        int sou = e[0];
-        int dest = e[1];
-
-        a[sou][dest] = 1;
-        a[dest][sou] = 1;
+        a[src][dest]=1;
+        a[dest][src]=1;
+    
     }
+bool ans=false;
 
-    vector<bool> visited(n, false);
-
-    bool ans = false;
-
-    for (int i = 0; i < n; i++) {
-        if (!visited[i]) {
-            if (dfs(a, i, -1, visited, n)) {
-                ans = true;
-                break;
-            }
+    for(int i=0;i<n;i++){
+        if(visited[i]==false){
+            ans=dfs(a,visited,i,-1,n);
+        }
+        if(ans==true){
+            
+            break;
         }
     }
-
-    cout << ans;
+    cout<<ans;
 
     return 0;
+
 }
